c27b8a4ead19d016ecf07ff29cb04a4401f7bc22
Jose Mendez <josemmendezt@gmail.com>
2024-02-04 20:35:36 -0700

    add suspense

--- src/app/(external)/signUp/page.tsx
+++ src/app/(external)/signUp/page.tsx
@@ -8,6 +8,7 @@ import { ChromeIcon, LinkedinIcon } from 'lucide-react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 export default function SignUp() {
   const router = useRouter();
@@ -25,62 +26,66 @@ export default function SignUp() {
   };
 
   return (
-    <section className="flex items-center justify-center h-[80vh]">
-      <div className="mx-auto max-w-[350px] space-y-6 ">
-        <div className="space-y-2 text-center">
-          <h1 className="text-3xl font-bold">Sign up to continue</h1>
-        </div>
-        <div>
-          <Card>
-            <CardContent className="space-y-4">
-              <div className="space-y-2 mt-4">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  placeholder="Enter your email"
-                  required
-                  type="email"
-                />
-              </div>
-              <Button
-                onClick={DoSignUp}
-                className="w-full"
-                type="submit"
-              >
-                Sign Up
-              </Button>
-              <div className="mt-2 flex w-full justify-between">
-                <Separator className="my-4 w-2/5" />
-                <span className="px-4 pt-1">Or</span>
-                <Separator className="my-4 w-2/5" />
-              </div>
-              <div className="space-y-4">
-                <Button className="w-full" variant="outline">
-                  Sign up with Google
-                  <ChromeIcon className="w-4 h-4 ml-2" />
-                </Button>
-                <Button className="w-full" variant="outline">
-                  Sign up with LinkedIn
-                  <LinkedinIcon className="w-4 h-4 ml-2" />
-                </Button>
+    <Suspense>
+      <section className="flex items-center justify-center h-[80vh]">
+        <div className="mx-auto max-w-[350px] space-y-6 ">
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold">
+              Sign up to continue
+            </h1>
+          </div>
+          <div>
+            <Card>
+              <CardContent className="space-y-4">
+                <div className="space-y-2 mt-4">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    placeholder="Enter your email"
+                    required
+                    type="email"
+                  />
+                </div>
                 <Button
-                  asChild
-                  variant="link"
-                  className=" text-muted-foreground text-center mx-auto flex justify-center"
+                  onClick={DoSignUp}
+                  className="w-full"
+                  type="submit"
                 >
-                  <Link href="/login">
-                    Already have an account? Log In
-                  </Link>
+                  Sign Up
                 </Button>
-                <Separator className="my-8" />
-                <Button className="w-full" variant="secondary">
-                  Continue as Guest
-                </Button>
-              </div>
-            </CardContent>
-          </Card>
+                <div className="mt-2 flex w-full justify-between">
+                  <Separator className="my-4 w-2/5" />
+                  <span className="px-4 pt-1">Or</span>
+                  <Separator className="my-4 w-2/5" />
+                </div>
+                <div className="space-y-4">
+                  <Button className="w-full" variant="outline">
+                    Sign up with Google
+                    <ChromeIcon className="w-4 h-4 ml-2" />
+                  </Button>
+                  <Button className="w-full" variant="outline">
+                    Sign up with LinkedIn
+                    <LinkedinIcon className="w-4 h-4 ml-2" />
+                  </Button>
+                  <Button
+                    asChild
+                    variant="link"
+                    className=" text-muted-foreground text-center mx-auto flex justify-center"
+                  >
+                    <Link href="/login">
+                      Already have an account? Log In
+                    </Link>
+                  </Button>
+                  <Separator className="my-8" />
+                  <Button className="w-full" variant="secondary">
+                    Continue as Guest
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Suspense>
   );
 }
