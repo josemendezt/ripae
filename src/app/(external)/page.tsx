fb0e5747ffafea6403325698771795c5a0dcaf91
Jose Mendez <josemmendezt@gmail.com>
2024-02-04 20:49:24 -0700

    fix prerender

--- src/app/(external)/page.tsx
+++ src/app/(external)/page.tsx
@@ -1,9 +1,93 @@
-import SignUp from './signUp/page';
+'use client';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { CardContent, Card } from '@/components/ui/card';
+import { Separator } from '@/components/ui/separator';
+import { ChromeIcon, LinkedinIcon } from 'lucide-react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+
+// Need to thing what to do with the home page, this is code duplicated from sighnUp
 
 export default function Home() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const flowType = searchParams.get('flow');
+  const DoSignUp = () => {
+    switch (flowType) {
+      case 'investor':
+        return router.push('/signUp/investor/personal');
+      case 'borrower':
+        return router.push('/signUp/borrower/personal');
+      default:
+        router.push('/signUp/accountSetUp');
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignUp />
-    </main>
+    <Suspense>
+      <section className="flex items-center justify-center h-[80vh]">
+        <div className="mx-auto max-w-[350px] space-y-6 ">
+          <div className="space-y-2 text-center">
+            <h1 className="text-3xl font-bold">
+              Sign up to continue
+            </h1>
+          </div>
+          <div>
+            <Card>
+              <CardContent className="space-y-4">
+                <div className="space-y-2 mt-4">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    id="email"
+                    placeholder="Enter your email"
+                    required
+                    type="email"
+                  />
+                </div>
+                <Button
+                  onClick={DoSignUp}
+                  className="w-full"
+                  type="submit"
+                >
+                  Sign Up
+                </Button>
+                <div className="mt-2 flex w-full justify-between">
+                  <Separator className="my-4 w-2/5" />
+                  <span className="px-4 pt-1">Or</span>
+                  <Separator className="my-4 w-2/5" />
+                </div>
+                <div className="space-y-4">
+                  <Button className="w-full" variant="outline">
+                    Sign up with Google
+                    <ChromeIcon className="w-4 h-4 ml-2" />
+                  </Button>
+                  <Button className="w-full" variant="outline">
+                    Sign up with LinkedIn
+                    <LinkedinIcon className="w-4 h-4 ml-2" />
+                  </Button>
+                  <Button
+                    asChild
+                    variant="link"
+                    className=" text-muted-foreground text-center mx-auto flex justify-center"
+                  >
+                    <Link href="/login">
+                      Already have an account? Log In
+                    </Link>
+                  </Button>
+                  <Separator className="my-8" />
+                  <Button className="w-full" variant="secondary">
+                    Continue as Guest
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
+      </section>
+    </Suspense>
   );
 }
