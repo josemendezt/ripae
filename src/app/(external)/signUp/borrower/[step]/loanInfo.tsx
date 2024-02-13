049eeb5a71170fc6af8cbee72c06327bfc6f686c
Jose Mendez <josemmendezt@gmail.com>
2024-02-12 21:00:23 -0700

    adjust borrower flow

--- src/app/(external)/signUp/borrower/[step]/loanInfo.tsx
+++ src/app/(external)/signUp/borrower/[step]/loanInfo.tsx
@@ -24,6 +24,10 @@ import { useRouter } from 'next/navigation';
 
 export default function LoanInfo() {
   const router = useRouter();
+  const amounts = [
+    250, 500, 750, 1000, 1500, 2000, 2500, 3000, 3500, 4000, 4500,
+    5000,
+  ];
 
   return (
     <Card className="w-full max-w-2xl">
@@ -73,6 +77,23 @@ export default function LoanInfo() {
               <SelectItem value="30">30 Days</SelectItem>
               <SelectItem value="60">60 Days</SelectItem>
               <SelectItem value="90">90 Days</SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
+        <div className="flex flex-col space-y-1.5">
+          <Label htmlFor="invest">
+            How much would you like to borrow?
+          </Label>
+          <Select>
+            <SelectTrigger>
+              <SelectValue placeholder="Select Amount" />
+            </SelectTrigger>
+            <SelectContent>
+              {amounts.map((amount) => (
+                <SelectItem key={amount} value={amount.toString()}>
+                  {amount}
+                </SelectItem>
+              ))}
             </SelectContent>
           </Select>
         </div>
--- src/app/(external)/signUp/stepsGuide.tsx
+++ src/app/(external)/signUp/stepsGuide.tsx
@@ -6,7 +6,7 @@ function StepsGuide({ steps }: { steps: string[] }) {
   const params = useParams<{ step: string }>();
 
   return (
-    <div className="flex items-center justify-center mx-auto mb-12">
+    <div className="flex items-center justify-center mx-auto mb-12 mt-20">
       {steps.map((stepVal, index) => (
         <>
           <div key={stepVal} className="flex items-center relative">
