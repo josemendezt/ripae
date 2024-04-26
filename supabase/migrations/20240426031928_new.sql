324e3c6168ad3239f4cdd44fff3372e834e5f5f7
Jose Mendez <josemmendezt@gmail.com>
2024-04-25 21:33:32 -0600

    fix migration

--- supabase/migrations/20240426031928_new.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."loans" add constraint "public_loans_borrower_id_fkey" FOREIGN KEY (borrower_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
-
-alter table "public"."loans" validate constraint "public_loans_borrower_id_fkey";
-
-
