4bb8deb5fcce235231e4713747e358ffb9c5b900
Jose Mendez <josemmendezt@gmail.com>
2024-04-27 13:41:28 -0600

    set favicons

--- src/app/(external)/layout.tsx
+++ src/app/(external)/layout.tsx
@@ -1,13 +1,17 @@
-import type { Metadata } from "next";
-import "../globals.css";
-import Header from "./ext-header";
+import type { Metadata } from 'next';
+import '../globals.css';
+import Header from './ext-header';
 
 export const metadata: Metadata = {
-  title: "Ripae App",
-  description: "Log in or sign up to get access to Ripae",
+  title: 'Ripae App',
+  description: 'Log in or sign up to get access to Ripae',
 };
 
-export default function ExtLayout({ children }: { children: React.ReactNode }) {
+export default function ExtLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
       <Header />
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,36 @@
-import "./globals.css";
+import './globals.css';
 export const metadata = {
-  title: "Welcome to Ripae!",
-  description: "Best peer to peer platform",
+  title: 'Welcome to Ripae!',
+  description: 'Best peer to peer platform',
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      url: '/favicons/favicon-32x32.png',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      url: '/favicons/favicon-16x16.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/favicons/apple-touch-icon.png',
+    },
+    {
+      rel: 'android-chrome',
+      sizes: '192x192',
+      url: '/favicons/android-chrome-192x192.png',
+    },
+    {
+      rel: 'android-chrome',
+      sizes: '512x512',
+      url: '/favicons/android-chrome-512x512.png',
+    },
+  ],
 };
 
 export default function RootLayout({
